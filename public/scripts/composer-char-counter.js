243e7d0c3b9d6425716b622e69ffc92295a2ec16
Tyler <t.mar92@gmail.com>
2019-10-08 13:52:50 +0000

    added AJAX GET request

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('.new-tweet textarea').on('keyup', function() {
+    let counter = $(this).siblings('.counter');
+    counter.text(140 - this.textLength);
+    if (counter.text() < 0) {
+      counter.addClass('over-limit');
+    } else if (counter.text() >= 0) {
+      counter.removeClass('over-limit');
+    }
+  }); //end on keyup
+});
